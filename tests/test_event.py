09824a2530966eb27dfa5611e66ce3737d36e5c0
Nathan Smith <nathan@keyba.se>
2019-09-05 18:52:00 -0400

    make the tests pass

--- tests/test_event.py
+++ tests/test_event.py
@@ -16,43 +16,50 @@ def fixture_path():
     return os.path.join(app_dir, "tests/fixtures")
 
 
-# def test_teamchat(fixture_path):
-#     with open(f"{fixture_path}/teamchat.json") as json_file:
-#         data = json.load(json_file)
+def test_teamchat(fixture_path):
+    with open(f"{fixture_path}/teamchat.json") as json_file:
+        data = json.load(json_file)
 
-#     event = KbEvent.from_dict(data)
+    event = KbEvent.from_dict(data)
 
-#     assert event.type == EventType.CHAT
-#     expected_team_mention = TeamMention(name="yourcompany.marketing", channel="general")
-#     assert event.msg.content.text.teamMentions == [expected_team_mention]
-#     reply_channel = event.msg.channel.replyable_dict()
-#     assert reply_channel == {
-#         "name": "yourcompany.marketing",
-#         "topic_name": "lunchtalk",
-#         "members_type": "team",
-#     }
+    assert event.type == EventType.CHAT
+    expected_team_mention = chat1.KnownTeamMention(
+        name="yourcompany.marketing", channel="general"
+    )
+    assert event.msg.content.text.team_mentions == [expected_team_mention]
+    reply_channel = event.msg.channel.to_dict()
+    assert reply_channel == {
+        "name": "yourcompany.marketing",
+        "topic_name": "lunchtalk",
+        "members_type": "team",
+        "topic_type": "chat",
+        "public": False,
+    }
 
 
-# def test_oneonone(fixture_path):
-#     with open(f"{fixture_path}/oneonone.json") as json_file:
-#         data = json.load(json_file)
+def test_oneonone(fixture_path):
+    with open(f"{fixture_path}/oneonone.json") as json_file:
+        data = json.load(json_file)
 
-#     event = KbEvent.from_dict(data)
+    event = KbEvent.from_dict(data)
 
-#     assert event.type == EventType.CHAT
-#     assert event.msg.content.text.body == "hi"
-#     reply_channel = event.msg.channel.replyable_dict()
-#     assert reply_channel == {"name": "someoneelse,yourbot"}
+    assert event.type == EventType.CHAT
+    assert event.msg.content.text.body == "hi"
+    reply_channel = event.msg.channel.to_dict()
+    assert reply_channel == {
+        "name": "someoneelse,yourbot",
+        "members_type": "impteamnative",
+        "public": False,
+        "topic_name": None,
+        "topic_type": "chat",
+    }
 
 
 def test_reaction(fixture_path):
     with open(f"{fixture_path}/reaction.json") as json_file:
         data = json.load(json_file)
 
-    print(data)
-    print(chat1.MessageType.REACTION.value)
     event = KbEvent.from_dict(data)
-    print(event.msg.content.type_name)
 
     assert event.msg.content.type_name == chat1.MessageTypeStrings.REACTION.value
     assert event.msg.content.reaction.body == ":sunglasses:"
